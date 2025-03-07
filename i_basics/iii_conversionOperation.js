0469ac49871644929fd091f99d35ad9c5c513c7f
Kumar Ravi <kr0837@srmist.edu.in>
2025-03-07 12:01:36 +0000

    studied further about dataTypes conversion

--- i_basics/iii_conversionOperation.js
+++ i_basics/iii_conversionOperation.js
@@ -26,3 +26,24 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+// ****************OPERATIONS*********************
+
+let value = 69
+let negValue = -value
+// console.log(negValue)
+
+
+// console.table([2+2, 2-2, 2*2, 2**3, 2/3, 2%3])
+
+
+//string concatenation
+let str1 = "hello"
+let str2 = " badassravi"
+let str3 = str1 + str2
+// console.log(str3);
+
+//string conversion scenarios
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2)
