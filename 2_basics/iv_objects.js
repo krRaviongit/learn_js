875400b89f6d56ff81e0be0b31de3912c39d9c96
Kumar Ravi <kr0837@srmist.edu.in>
2025-06-15 15:10:40 +0000

    studied objects using constructors and some methods on objects

---
+++ 2_basics/iv_objects.js
@@ -0,0 +1,81 @@
+// const tinderUser = new Object()   //singleton object
+const tinderUser = {}     //non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "kumar",
+            lastname: "ravi"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 } // this is not the correct way
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // correct way and {} is the optional parameter here
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));  //entries are used to make each elements in the form of array consisting of key and value separated by commas inside an array.
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //hasOwnProperty() checks whether the object has that property or not ,it returns boolean values 
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
